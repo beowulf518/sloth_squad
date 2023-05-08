585292a48541364a29acd1145d2f98e12b404c17
Eberechi <eberewill@gmail.com>
2023-05-08 22:28:41 +0100

    update logo

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,11 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>Sloth Squard</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="SlothSquad is a community-driven project dedicated to supporting sloth conservation efforts. Our team is made up of passionate individuals who are committed to making a positive impact on the world." />
+      <meta name="keywords" content="sloth, conservation, cryptocurrency, community-driven, slothsquad" />
+      <title>SlothSquad - Supporting Sloth Conservation Efforts with Cryptocurrency</title>
       <link rel="icon" href="/favicon.ico" />
+
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </Head>
